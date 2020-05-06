74b61121e77906ee4d9e1fdae29c752f78eb24b2
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-05-06 13:21:22 +0300

    ex 1 done

--- part1/Program.cs
+++ part1/Program.cs
@@ -2,11 +2,17 @@
 
 namespace part1
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      // Console.WriteLine("Hello World!");
+
+      Numbers num = new Numbers();
+      Console.WriteLine(num.Sum(4075)); // 16
+      Console.WriteLine(num.Sum(3)); // 3
+      Console.WriteLine(num.Sum(999999999)); // 81
+
     }
+  }
 }
