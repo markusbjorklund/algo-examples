5b5aa584bcfeb6c95a4ac589867819db4f06b8f3
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-05-09 10:57:51 +0300

    ex 3 done

--- part1/Program.cs
+++ part1/Program.cs
@@ -13,10 +13,16 @@ namespace part1
       // Console.WriteLine(num.Sum(3)); // 3
       // Console.WriteLine(num.Sum(999999999)); // 81
 
-      Substrings subs = new Substrings();
-      Console.WriteLine(subs.Calculate("aybabtu", "bab")); // 1
-      Console.WriteLine(subs.Calculate("aaaaa", "aa")); // 4
-      Console.WriteLine(subs.Calculate("monkey", "banana")); // 0
+      // Substrings subs = new Substrings();
+      // Console.WriteLine(subs.Calculate("aybabtu", "bab")); // 1
+      // Console.WriteLine(subs.Calculate("aaaaa", "aa")); // 4
+      // Console.WriteLine(subs.Calculate("monkey", "banana")); // 0
+
+      Tables t = new Tables();
+      Console.WriteLine(t.Calculate(new int[] { 1, 2, 3, 2 })); // 18
+      Console.WriteLine(t.Calculate(new int[] { 5 })); // 5
+      Console.WriteLine(t.Calculate(new int[] { 4, 2, 9, 1, 9, 2, 5 })); // 323
+
     }
   }
 }
--- part1/exercise3.cs
+++ part1/exercise3.cs
@@ -4,7 +4,33 @@ namespace part1
   {
     public int Calculate(int[] t)
     {
-      return 0;
+
+      /*
+
+if t.Length == 1
+return value of the only thing in t
+else
+create new array, int[t.Length-1]
+for each in the original array
+do the calculations, do not overflow
+return Calculate(new array)*/
+
+// solution from example during class
+
+      if (t.Length == 1)
+      {
+        return t[0];
+      }
+
+      else
+      {
+        int[] newArray = new int[t.Length - 1];
+        for (int i = 0; i < t.Length - 1; i++)
+        {
+          newArray[i] = t[i] + t[i + 1];
+        }
+        return Calculate(newArray);
+      }
     }
   }
 }
