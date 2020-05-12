1e118caa677fcf73b24f9c3df32f2f28b8071c12
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-05-12 18:41:33 +0300

    failed on 4

--- part1/Program.cs
+++ part1/Program.cs
@@ -18,11 +18,15 @@ namespace part1
       // Console.WriteLine(subs.Calculate("aaaaa", "aa")); // 4
       // Console.WriteLine(subs.Calculate("monkey", "banana")); // 0
 
-      Tables t = new Tables();
-      Console.WriteLine(t.Calculate(new int[] { 1, 2, 3, 2 })); // 18
-      Console.WriteLine(t.Calculate(new int[] { 5 })); // 5
-      Console.WriteLine(t.Calculate(new int[] { 4, 2, 9, 1, 9, 2, 5 })); // 323
+      // Tables t = new Tables();
+      // Console.WriteLine(t.Calculate(new int[] { 1, 2, 3, 2 })); // 18
+      // Console.WriteLine(t.Calculate(new int[] { 5 })); // 5
+      // Console.WriteLine(t.Calculate(new int[] { 4, 2, 9, 1, 9, 2, 5 })); // 323
 
+      LuckyNumbers luck = new LuckyNumbers();
+      Console.WriteLine(luck.Calculate(1, 10)); // 2
+      Console.WriteLine(luck.Calculate(123, 321)); // 0
+      Console.WriteLine(luck.Calculate(1, 1000000)); // 126
     }
   }
 }
--- part1/exercise4.cs
+++ part1/exercise4.cs
@@ -1,9 +1,32 @@
+using System;
+using System.Collections.Generic;
+
 namespace part1
 {
   public class LuckyNumbers
   {
     public int Calculate(int a, int b)
     {
+      // An integer is a lucky number, if every number in it is either 3 or 7. For example, 3, 7, 33, 37, 73, 77, and 733737 are lucky numbers. Your assigment is to calculate lucky numbers between a…b.
+
+      // HINT! Calculate from 1 to a and 1 to b separately, and subtract them.
+      // HINT! Do not go through all the numbers, it is very inefficient. Try to find a mathematical pattern! 
+
+      // Console.WriteLine(luck.Calculate(1, 10)); // 2
+      // Console.WriteLine(luck.Calculate(123, 321)); // 0
+      // Console.WriteLine(luck.Calculate(1, 1000000)); // 126
+
+      // 3, 7, 33, 37, 73, 77, 333, 337, 373, 377, 733, 737, 777, 3333, 3337
+
+      /*
+
+      3, 33, 37, 333, 337, 373, 377, 3333, 3337
+
+      7, 73, 77, 733, 737, 773, 777, 7333, 7337 
+
+      */
+
+      // int luckyNumbers = 0;
       return 0;
     }
   }
