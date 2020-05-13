f889b6e5958f1d358e817970105287c1c0ebae47
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-05-13 11:33:11 +0300

    no spaghetti just proper code

--- part1/exercise4.cs
+++ part1/exercise4.cs
@@ -1,33 +1,47 @@
 using System;
 using System.Collections.Generic;
-
 namespace part1
 {
   public class LuckyNumbers
   {
     public int Calculate(int a, int b)
     {
-      // An integer is a lucky number, if every number in it is either 3 or 7. For example, 3, 7, 33, 37, 73, 77, and 733737 are lucky numbers. Your assigment is to calculate lucky numbers between a…b.
+      return CountLuckyNumbers(b) - CountLuckyNumbers(a - 1);
+    }
+    private int CountLuckyNumbers(int input)
+    {
+      List<int> numbers = new List<int>();
+      if (input >= 3)
+      {
+        numbers.Add(3);
+      }
+      else return numbers.Count;
+      if (input >= 7)
+      {
+        numbers.Add(7);
+      }
+      else return numbers.Count;
 
-      // HINT! Calculate from 1 to a and 1 to b separately, and subtract them.
-      // HINT! Do not go through all the numbers, it is very inefficient. Try to find a mathematical pattern! 
-
-      // Console.WriteLine(luck.Calculate(1, 10)); // 2
-      // Console.WriteLine(luck.Calculate(123, 321)); // 0
-      // Console.WriteLine(luck.Calculate(1, 1000000)); // 126
-
-      // 3, 7, 33, 37, 73, 77, 333, 337, 373, 377, 733, 737, 777, 3333, 3337
-
-      /*
-
-      3, 33, 37, 333, 337, 373, 377, 3333, 3337
-
-      7, 73, 77, 733, 737, 773, 777, 7333, 7337 
-
-      */
-
-      // int luckyNumbers = 0;
-      return 0;
+      // counting with variables and adding to list
+      // a loop solution to remember for future use
+      int i = 0;
+      while(true)
+      {
+        int varPlusThree = numbers[i] * 10 + 3;
+        int varPlusSeven = numbers[i] * 10 + 7;
+        if (varPlusThree > input)
+        {
+          break;
+        }
+        numbers.Add(varPlusThree);
+        if (varPlusThree > input)
+        {
+          break;
+        }
+        numbers.Add(varPlusSeven);
+        i++; 
+      }
+      return numbers.Count;
     }
   }
 }
