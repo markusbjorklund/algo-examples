b348e966f1fe72e27092d300c8a263de97faa96f
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-05-18 11:14:18 +0300

    ex 3 done

--- part2/Program.cs
+++ part2/Program.cs
@@ -9,11 +9,20 @@ namespace part2
   {
     static void Main(string[] args)
     {
+
+      // Changes
+      Changes m = new Changes();
+      Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 2 })); // 2
+      Console.WriteLine(m.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
+      Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 2
+      Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 3, 3, 3 })); // 2
+
       // Repetition
-      LongestRepetition p = new LongestRepetition();
-      Console.WriteLine(p.Calculate(new int[] { 1, 2, 1, 1, 2 })); // 2
-      Console.WriteLine(p.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
-      Console.WriteLine(p.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 5
+      // LongestRepetition p = new LongestRepetition();
+      // Console.WriteLine(p.Calculate(new int[] { 1, 2, 1, 1, 2 })); // 2
+      // Console.WriteLine(p.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
+      // Console.WriteLine(p.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 5
+      // Console.WriteLine(p.Calculate(new int[] { 2, 2, 2, 1, 1 })); // 5
 
       // // Time efficiency
       // // O(n^2) is named Turtle
@@ -37,20 +46,19 @@ namespace part2
       // Console.WriteLine("=======================================");
       // Console.WriteLine("Rabbit total time: " + end2.Subtract(start2));
       // Console.WriteLine("=======================================");
-//       DateTime start = DateTime.Now;
-//       counter.Turtle(tenthousand);
-//       DateTime end = DateTime.Now;
-//       Console.WriteLine("=======================================");
-//       Console.WriteLine("Turtle total time: " + end.Subtract(start));
-//       Console.WriteLine("=======================================");
+      //       DateTime start = DateTime.Now;
+      //       counter.Turtle(tenthousand);
+      //       DateTime end = DateTime.Now;
+      //       Console.WriteLine("=======================================");
+      //       Console.WriteLine("Turtle total time: " + end.Subtract(start));
+      //       Console.WriteLine("=======================================");
 
-//       DateTime start2 = DateTime.Now;
-//       counter.Rabbit(tenthousand);
-//       DateTime end2 = DateTime.Now;
-//       Console.WriteLine("=======================================");
-//       Console.WriteLine("Rabbit total time: " + end2.Subtract(start2));
-//       Console.WriteLine("=======================================");
-// >>>>>>> c4fdeb4becca8aa15237e9df04cb04ae15bf12a7
+      //       DateTime start2 = DateTime.Now;
+      //       counter.Rabbit(tenthousand);
+      //       DateTime end2 = DateTime.Now;
+      //       Console.WriteLine("=======================================");
+      //       Console.WriteLine("Rabbit total time: " + end2.Subtract(start2));
+      //       Console.WriteLine("=======================================");
 
       // Console.WriteLine("In milliseconds: " + (end.Ticks - start.Ticks) / 10000.0);
       // Console.WriteLine("In seconds: " + (end.Ticks - start.Ticks) / 10000000.0);
