fbe4d5878ac6474cae98734731dd6ddf46d804d7
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-05-18 11:54:42 +0300

    ex 4 done

--- part2/Program.cs
+++ part2/Program.cs
@@ -9,13 +9,18 @@ namespace part2
   {
     static void Main(string[] args)
     {
+      // Revolutions
+      Revolutions r = new Revolutions();
+      Console.WriteLine(r.Calculate(new int[] { 4, 1, 3, 2, 5 })); // 3
+      Console.WriteLine(r.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
+      Console.WriteLine(r.Calculate(new int[] { 5, 4, 3, 2, 1 })); // 5
 
       // Changes
-      Changes m = new Changes();
-      Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 2 })); // 2
-      Console.WriteLine(m.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
-      Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 2
-      Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 3, 3, 3 })); // 2
+      // Changes m = new Changes();
+      // Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 2 })); // 2
+      // Console.WriteLine(m.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
+      // Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 2
+      // Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 3, 3, 3 })); // 2
 
       // Repetition
       // LongestRepetition p = new LongestRepetition();
