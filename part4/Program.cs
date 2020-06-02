30af812120598abd6f060e3e12d8d3a8fda06588
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-06-02 12:11:44 +0300

    ex4 in progress

--- part4/Program.cs
+++ part4/Program.cs
@@ -23,16 +23,63 @@ namespace part4
       // s.Add(9);
       // Console.WriteLine(s.Calculate()); // 1
 
-      LinkedList myLinks = new LinkedList();
-      myLinks.AddLast(1);
-      myLinks.AddFirst(2);
-      myLinks.AddLast(3);
-      Console.WriteLine(myLinks); // for example: 2, 1, 3
-      myLinks.RemoveFirst();
-      Console.WriteLine(myLinks); // for example: 1, 3
-      Console.WriteLine(myLinks.GetNode(0)); // 1
-      Console.WriteLine(myLinks.GetNode(1)); // 3
+      // LinkedList myLinks = new LinkedList();
+      // myLinks.AddLast(1);
+      // myLinks.AddFirst(2);
+      // myLinks.AddLast(3);
+      // Console.WriteLine(myLinks); // for example: 2, 1, 3
+      // myLinks.RemoveFirst();
+      // Console.WriteLine(myLinks); // for example: 1, 3
+      // Console.WriteLine(myLinks.GetNode(0)); // 1
+      // Console.WriteLine(myLinks.GetNode(1)); // 3
 
+      // Order o = new Order();
+      // int[] a = { 4, 2, 1, 3, 5 };
+      // int[] b = { 2, 4, 3, 1, 5 };
+      // int[] c = o.Create(a, b);
+      // Console.WriteLine(String.Join(" ", c)); // 2 3 5 1 4
+
+      // BinarySearchTree bs = new BinarySearchTree();
+      // bs.Add(5); // Becomes your root
+      // bs.Add(4); // Goes to the left
+      // bs.Add(6); // Goes to the right
+      // Console.WriteLine(bs.Height()); // 1
+      // bs.Add(3);
+      // bs.Add(1);
+      // bs.Add(7);
+      // Console.WriteLine(bs.Height()); // 3
+
+      static void RandomNodes(BinarySearchTree tree)
+      {
+        // Quite efficient up to 100000
+        int hundrerThousand = 100000;
+        Random random = new Random();
+        List<int> nodeList = new List<int>();
+        // Add all the numbers to the nodelist
+        for (int i = 1; i < hundrerThousand + 1; i++)
+        {
+          nodeList.Add(i);
+        }
+        // Add them to the tree in random order
+        while (nodeList.Count > 1)
+        {
+          // All the numbers left at the list
+          int remove = random.Next(1, nodeList.Count);
+          // Add from the index "remove"
+          tree.Add(nodeList[remove]);
+          // remove the number from index "remove"
+          nodeList.RemoveAt(remove);
+        }
+        // Add the last one, remove from the list
+        tree.Add(nodeList[0]);
+        nodeList.RemoveAt(0);
+      }
+
+      BinarySearchTree bs = new BinarySearchTree();
+      RandomNodes(bs);
+      Console.WriteLine(bs.Height());
+      // Most probably around 40, +/- something
+      // With 100000 Nodes
     }
   }
 }
