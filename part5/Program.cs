c69e43c1c655b289b2f4f78a9cbcef63c346d245
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-06-09 12:29:43 +0300

    ex5 not done

---
+++ part5/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace part5
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Connectivity c = new Connectivity(6);
+      // c.AddConnection(1, 2);
+      // c.AddConnection(2, 3);
+      // c.AddConnection(1, 3);
+      // c.AddConnection(3, 4);
+      // c.AddConnection(5, 6);
+      // Console.WriteLine(c.Calculate(1)); // 3
+
+      // Components k = new Components(6);
+      // k.AddConnection(1, 2);
+      // k.AddConnection(2, 3);
+      // k.AddConnection(1, 3);
+      // k.AddConnection(3, 4);
+      // k.AddConnection(5, 6);
+      // Console.WriteLine(k.Calculate()); // 2
+
+      // Communication com = new Communication(6);
+      // com.AddConnection(1, 2);
+      // com.AddConnection(2, 3);
+      // com.AddConnection(1, 3);
+      // com.AddConnection(3, 4);
+      // com.AddConnection(5, 6);
+      // Console.WriteLine(com.Examine(1, 4)); // true
+      // Console.WriteLine(com.Examine(2, 5)); // false
+      // Console.WriteLine(com.Examine(5, 6)); // true
+
+      Labyrinth l = new Labyrinth();
+      char[, ] c = { { '#', '#', '#', '#', '#', '#', '#' },
+        { '#', 'x', '#', '.', 'y', '.', '#' },
+        { '#', '.', '#', '.', '#', '.', '#' },
+        { '#', '.', '.', '.', '.', '.', '#' },
+        { '#', '#', '#', '#', '#', '#', '#' }
+      };
+      Console.WriteLine(l.Search(c)); // DDRRUUR
+    }
+  }
+}
---
+++ part5/exercise5.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace part5
+{
+  class Labyrinth
+  { 
+    public List<string> route; 
+    public List<string> visited;
+
+    public string Search(char[,] laby)
+    {
+      return null;
+    }
+  }
+}
