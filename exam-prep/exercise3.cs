e030b64be0bdc312a5a7214090bc9036a9a76b6b
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-22 22:02:13 +0300

    not done

--- exam-prep/exercise3.cs
+++ exam-prep/exercise3.cs
@@ -7,7 +7,8 @@ namespace examprep
   {
     public int Calculate(int[, ] input)
     {
-
+      
+      return 0;
       // You are given a map of rooms in a house, and your task is to calculate, how many rooms a house has. A map is a grid where each square is either floor (marked with 0) or wall (marked with 1). Two floor squares are in the same room, if they are adjacent horizontally or vertically. You can assume each square on the outer perimeter is wal
 
     }
