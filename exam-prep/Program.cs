8090c2a7aecf144ae1180468f6d96d86c9f6c1d9
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-22 22:03:11 +0300

    beepbeep

--- exam-prep/Program.cs
+++ exam-prep/Program.cs
@@ -16,22 +16,24 @@ namespace examprep
       // Console.WriteLine(num.Steps(15)); // 5
       // Console.WriteLine(num.Steps(2)); // 2
 
-      // Console.WriteLine("");
-      // Console.WriteLine("#####");
+      Console.WriteLine("");
+      Console.WriteLine("#####");
+      Console.WriteLine("");
 
-      // int[] array1 = { 1, 2, 2, 1, 1, 1, 3 };
-      // int[] array2 = { 1, 2, 3, 4, 5, 6, 7, 8 };
-      // int[] array3 = { 1, 1, 1, 1, 2, 2, 2, 2 };
-      // int[] array4 = { 1, 2, 3, 4, 4, 3, 2, 1 };
-      // int[] array5 = { 1, 2, 1, 1, 2, 2, 1, 1, 2, 2, 1 };
-      // Console.WriteLine(s.Calculate(array1)); // 1
-      // Console.WriteLine(s.Calculate(array2)); // 8
-      // Console.WriteLine(s.Calculate(array3)); // 0
-      // Console.WriteLine(s.Calculate(array4)); // 0
-      // Console.WriteLine(s.Calculate(array5)); // 3
+      ShortenArray s = new ShortenArray();
+      int[] array1 = { 1, 2, 2, 1, 1, 1, 3 };
+      int[] array2 = { 1, 2, 3, 4, 5, 6, 7, 8 };
+      int[] array3 = { 1, 1, 1, 1, 2, 2, 2, 2 };
+      int[] array4 = { 1, 2, 3, 4, 4, 3, 2, 1 };
+      int[] array5 = { 1, 2, 1, 1, 2, 2, 1, 1, 2, 2, 1 };
+      Console.WriteLine(s.Calculate(array1)); // 1
+      Console.WriteLine(s.Calculate(array2)); // 8
+      Console.WriteLine(s.Calculate(array3)); // 0
+      Console.WriteLine(s.Calculate(array4)); // 0
+      Console.WriteLine(s.Calculate(array5)); // 3
 
-      // Console.WriteLine("");
-      // Console.WriteLine("#####");
+      Console.WriteLine("");
+      Console.WriteLine("#####");
 
       // Rooms r = new Rooms();
       // int[,] house = {
